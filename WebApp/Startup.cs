abdf05905bc8a75f926d96aed5083dd5003bd113
ygrenne <ygrenne@gmail.com>
2016-11-09 13:00:39 +0200

    Integrating Simple Injector

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -5,16 +5,23 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SimpleInjector;
+using SimpleInjector.Integration.AspNetCore;
+using SimpleInjector.Integration.AspNetCore.Mvc;
 
 namespace Klash.WebApp
 {
     public class Startup
     {
+        Container container = new Container();
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddSingleton<IControllerActivator>(new SimpleInjectorControllerActivator(container));
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
@@ -28,10 +35,23 @@ namespace Klash.WebApp
 
             app.UseMvc();
 
+            app.UseSimpleInjectorAspNetRequestScoping(container);
+            InitializeContainer(app);
+            container.Verify();
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello Mazafaka!");
             });
+        }
+
+        private void InitializeContainer(IApplicationBuilder app)
+        {
+            container.Options.DefaultScopedLifestyle = new AspNetRequestLifestyle();
+
+            container.RegisterMvcControllers(app);
+            container.Register<ILogger, CustomLogger>(Lifestyle.Singleton);
+            container.RegisterSingleton(app.ApplicationServices.GetService<ILoggerFactory>());
         }
     }
 }
